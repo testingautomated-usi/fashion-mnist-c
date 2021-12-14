298df9f534a0b12d305d90068846ecb8bf9adc93
Michael <code@mweiss.ch>
2021-12-14 15:36:59 +0100

    safety push

---
+++ additional_corruptions.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def flip_sides(image):
+    """Flip the image left to right."""
+    return np.fliplr(image)
+
+
+def flip_up_down(image):
+    """Flip the image upside down."""
+    return np.flipud(image)
+
+
+def turn_left(image):
+    """Turn the image 90 degrees to the left."""
+    return np.rot90(image)
+
+
+def turn_right(image):
+    """Turn the image 90 degrees to the right."""
+    return np.rot90(image, 3)
